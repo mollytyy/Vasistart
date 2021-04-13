3bd108d4078979245bdb14cfc3931ec577692850
mollytyy <moty0519@gmail.com>
2021-04-13 01:04:55 -0500

    welcome screens xml

--- app/src/main/java/com/example/vasistartapp/MainActivity.java
+++ app/src/main/java/com/example/vasistartapp/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.vasistartapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button pair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pair = (Button) findViewById(R.id.pair);
     }
+
 }
